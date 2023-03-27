e303dc577b6d5af99d7404951c3c0671a8a26b8c
Seth Reid <reid.seth.96@gmail.com>
2023-03-27 16:58:46 -0600

    added partial js, refined a bit

--- client/home.js
+++ client/home.js
@@ -1,2 +1,42 @@
-let getFortune = document.getElementById('get-fortune')
-let askQuestion = document.getElementById('ask-question')
+const fortuneBtn = document.getElementById('get-fortune')
+const questionform = document.getElementById('question-form')
+const questionText = document.getElementById('user-question')
+const allCards = document.getElementById('show-all')
+
+let url = 'http://localhost:4000'
+
+
+function showCards(){
+    axios.get(`${url}/cards`)
+    .then((result) => {
+
+    })
+}
+
+function getFortune(e){
+    e.preventDefault()
+
+    axios.post(`${url}/fortune`)
+    .then((result) => {
+
+    })
+}
+
+function askQuestion(e){
+    e.preventDefault()
+
+    let body = {
+        question: questionText.value
+    }
+
+    axios.post(`${url}/question`, body)
+    .then((result) => {
+
+    })
+}
+
+
+allCards.addEventListener('click', showCards)
+fortuneBtn.addEventListener('click', getFortune)
+questionform.addEventListener('submit', askQuestion)
+
--- server/server.js
+++ server/server.js
@@ -1,3 +1,16 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+app.use(express.static(`${__dirname}/client`))
+
+const {} = require('controller.js')
 
 
 
+
+
+
+
+
+app.listen(4000, () => console.log('Up on 4000'))
